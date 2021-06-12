07e890439913eb160f0568a2467bcab165309482
Shubhanshu Arya <45733409+Shubhanshuarya@users.noreply.github.com>
2021-06-12 17:54:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -7,6 +6,4 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at ${port}/`);
-});
+server.listen(process.env.PORT);
